0f6f73b6073f8a0d58ba9ff9f25ef74dd2c63a0b
Yaniv M <yyaanniivv@users.noreply.github.com>
2020-10-13 18:34:37 +0300

    Updates

---
+++ precache-manifest.b19f67da81bbb9c0f94f0b990344f9dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8ca4172aa7d3dfb820e8b8aea081cc",
+    "url": "/math/index.html"
+  },
+  {
+    "revision": "3de993956b98c250f1ee",
+    "url": "/math/static/css/main.c220a940.chunk.css"
+  },
+  {
+    "revision": "ed2dc5a0a10f60b364af",
+    "url": "/math/static/js/2.511aae2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/math/static/js/2.511aae2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de993956b98c250f1ee",
+    "url": "/math/static/js/main.fac9ba86.chunk.js"
+  },
+  {
+    "revision": "398652c95f88d42e5f89",
+    "url": "/math/static/js/runtime-main.dc92f23e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/math/static/media/logo.5d5d9eef.svg"
+  }
+]);
