5a4f7ae1a7300a7e357cd4079b25742319efee57
Yaniv M <yyaanniivv@users.noreply.github.com>
2020-10-19 13:24:26 +0300

    WIP, Added Material UI slider to settings

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,5 +1,7 @@
 import React, { Dispatch, SetStateAction, useState } from "react";
+import Slider from "@material-ui/core/Slider";
 import { IConfig } from "./common";
+
 import "./Settings.css";
 
 interface Props {
@@ -10,17 +12,60 @@ interface Props {
 function Settings({ config, setConfig }: Props) {
   let [toggle, setToggle] = useState<boolean>(false);
 
-  const onConfigChange = ({ target: { id, value } }: React.ChangeEvent<HTMLInputElement>) => {
-    setConfig({ ...config, [id]: parseInt(value) });
+  const onTestChange = (name: string) => (_: any, values: Array<number> | number) =>
+    onConfigChange(name, values);
 
-    // debugger
+  const onConfigChange = (id: string, values: Array<number> | number) => {
+    let offset, range;
+    switch (id) {
+      case "aValues":
+        offset = (values as Array<number>)[0];
+        range = (values as Array<number>)[1];
+        console.log("a");
+
+        range = range - offset + 1;
+        setConfig({ ...config, aOffset: offset, aRange: range });
+        break;
+      // case "bValues":
+      //   console.log("B");
+
+      // offset = (values as Array<number>)[0];
+      // range = (values as Array<number>)[1];
+
+      //   range = range - offset + 1;
+      //   setConfig({ ...config, bOffset: offset, bRange: range });
+      //   break;
+      default:
+        console.log("error in switch case");
+    }
     // if (id === "action") {
     //   setConfig({ ...config, [id]: value });
     // } else {
     //   setConfig({ ...config, [id]: parseInt(value) });
     // }
   };
-
+  const marks = [
+    {
+      value: 0,
+      label: "0",
+    },
+    {
+      value: 20,
+      label: "20",
+    },
+    {
+      value: 40,
+      label: "40",
+    },
+    {
+      value: 60,
+      label: "60",
+    },
+    {
+      value: 80,
+      label: "80",
+    },
+  ];
   return (
     <div>
       <div onClick={() => setToggle(!toggle)}>{toggle ? "סגור הגדרות ^" : "פתח הגדרות V"}</div>
@@ -36,40 +81,28 @@ function Settings({ config, setConfig }: Props) {
           </div>
 
           <div className="input-group">
-            <div>
-              <label htmlFor="aOffset">aOffset</label>
-              <input
-                id="aOffset"
-                type="number"
-                value={config["aOffset"]}
-                onChange={onConfigChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="aRange">aRange</label>
-              <input id="aRange" type="number" value={config["aRange"]} onChange={onConfigChange} />
-            </div>
-            <div>
-              a = {config["aOffset"]} - {config["aOffset"] + config["aRange"] - 1}
-            </div>
+            <label htmlFor="aOffset">a range</label>
+            <Slider
+              value={[config["aOffset"], config["aOffset"] + config["aRange"] - 1]}
+              onChange={onTestChange("aValues")}
+              valueLabelDisplay="on"
+              aria-labelledby="range-slider"
+              marks={marks}
+              step={5}
+            />
+            a = {config["aOffset"]} - {config["aOffset"] + config["aRange"] - 1}
           </div>
           <div className="input-group">
-            <div>
-              <label htmlFor="bOffset">bOffset</label>
-              <input
-                id="bOffset"
-                type="number"
-                value={config["bOffset"]}
-                onChange={onConfigChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="bRange">bRange</label>
-              <input id="bRange" type="number" value={config["bRange"]} onChange={onConfigChange} />
-            </div>
-            <div>
-              b = {config["bOffset"]} - {config["bOffset"] + config["bRange"] - 1}
-            </div>
+            <label htmlFor="bOffset">bOffset</label>
+            <Slider
+              value={[config["bOffset"], config["bOffset"] + config["bRange"] - 1]}
+              onChange={onTestChange("bValues")}
+              valueLabelDisplay="on"
+              aria-labelledby="range-slider"
+              marks={marks}
+              step={5}
+            />
+            b = {config["bOffset"]} - {config["bOffset"] + config["bRange"] - 1}
           </div>
         </div>
       )}
